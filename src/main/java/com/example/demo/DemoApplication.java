9ca0008b20036d0d74d6e98464b17022d64f91ae
rparyian <roman.paryian@gmail.com>
2021-02-23 14:43:52 +0300

    ufu

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 	@RequestMapping("/")
